7b1848e396d8954b57743334b61654106fa87149
Nazrulla Sadullaev <nsadullaev98@gmail.com>
2020-08-28 23:48:47 +0500

    Add users migration

---
+++ app/Http/Controllers/User/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+  public function register(Request $request)
+  {
+    
+  }
+}
